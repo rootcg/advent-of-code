f861b49c6a1f94da98f552125c8ae6f2875aa00b
rootcg <cristian.gonzalez.morante@gmail.com>
2022-03-20 19:18:08 +0100

    start day 15

---
+++ src/day15/Day15.kt
@@ -0,0 +1,24 @@
+package day15
+
+import readInput
+import readTestInput
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readTestInput("Day15")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day15")
+    println(part1(input))
+    println(part2(input))
+}
